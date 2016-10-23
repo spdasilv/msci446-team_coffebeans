12f1e1a3f4111bea4f7e6bd717a6508a30d128c5
spdasilv <stefannopolo@yahoo.com.br>
2016-10-22 19:19:55 -0500

    Functional Unverted Index

--- Coffe_Beans_Inverted_Index/Coffe_Beans_Inverted_Index/Program.cs
+++ Coffe_Beans_Inverted_Index/Coffe_Beans_Inverted_Index/Program.cs
@@ -11,22 +11,92 @@ namespace Coffe_Beans_Inverted_Index
     {
         static void Main(string[] args)
         {
-            string[] stopWords = Stop_Words("stop_words.txt");
+            string[] stopWords = Stop_Words("Stop Words.txt");
 
-            StreamReader read_subjects = new StreamReader("testsubjdesc.txt");
+            StreamReader read_subjects = new StreamReader("Subject_DescV2.txt");
             string line = read_subjects.ReadLine();
             string curr_subject = "";
+            string[] curr_line = null;
+            Dictionary<string, Dictionary<string, int>> subject_Collection = new Dictionary<string, Dictionary<string, int>>();
+
             while (line != null)
             {
-                //CODE HERE
+                curr_line = line.Split();
 
+                if (curr_line[0] == "<SUBJECT>")
+                {
+                    curr_subject = curr_line[1];
+                    if (curr_subject == ",")
+                    {
+                        Console.WriteLine("FOUND IT!");
+                    }
+                    subject_Collection.Add(curr_subject, new Dictionary<string, int>());
+                }
+                else if (curr_line[0] == "</SUBJECT>")
+                {
+                    line = read_subjects.ReadLine();
+                    continue;
+                }
+                else
+                {
+                    for (int i = 0; i < curr_line.Length; i = i + 1)
+                    {
+                        if (Array.Exists(stopWords, element => element == curr_line[i].ToLower()) == false)
+                        {
+                            if (subject_Collection[curr_subject].ContainsKey(curr_line[i].ToLower()))
+                            {
+                                ++subject_Collection[curr_subject][curr_line[i].ToLower()];
+                            }
+                            else
+                            {
+                                subject_Collection[curr_subject].Add(curr_line[i].ToLower(), 1);
+                            }
+                        }
+                    }
+                }
                 line = read_subjects.ReadLine();
             }
+            read_subjects.Close();
+
+            Dictionary<string, Dictionary<string, int>> inverted_Index = new Dictionary<string, Dictionary<string, int>>();
+            foreach(KeyValuePair<string, Dictionary <string, int>> entry in subject_Collection)
+            {
+                string subject_Name = entry.Key;
+                foreach(KeyValuePair<string, int> term in entry.Value)
+                {
+                    if (inverted_Index.ContainsKey(term.Key))
+                    {
+                        inverted_Index[term.Key].Add(subject_Name, term.Value);
+                    }
+                    else
+                    {
+
+                        inverted_Index.Add(term.Key, new Dictionary<string, int>
+                        {
+                            {subject_Name, term.Value }
+                        });
+                    }
+                }
+            }
+
+            StreamWriter w = new StreamWriter("inverted_index.txt");
+            foreach (KeyValuePair< string, Dictionary < string, int>> term in inverted_Index)
+            {
+                w.Write(term.Key + " : ");
+                foreach(KeyValuePair<string,int> subject in term.Value)
+                {
+                    w.Write(subject.Key + "|" + subject.Value + "; ");
+                }
+                w.WriteLine();
+            }
+
+            Console.WriteLine("DONE");
+
         }
 
         static string[] Stop_Words (string file_name)
         {
-            //array to store all the stop wordsas
+            //array to store all the stop word
             string[] stopWords = new string[416];
             StreamReader sr1 = new StreamReader(file_name);
             string line;
